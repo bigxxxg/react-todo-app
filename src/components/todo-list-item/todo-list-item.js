22d349f0a4b21c75a0121757c1cf8fb831d30083
bigxxxg <dp070593gda@gmail.com>
2019-12-10 23:24:00 +0300

    Добавление цепочки функций для отслеживания состояния элементов,его изменения или удаления

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,35 +4,55 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+   state = {
+    done: false,
+    important: false
+  };
+
   onLabelClick = ()=> {
-      console.log(`Done: ${this.props.label}`)
-    }
+    this.setState({
+      done: true
+    });
+
+  };
+  OnMarkImportant = () => {
+    this.setState({
+      important: true
+    });
+  };
 
 
   render() {
-    const { label, important = false } = this.props;
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
 
+    let classNames = 'todo-list-item';
+
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += " important"
+    }
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
       <span
         className="todo-list-item-label"
-        style={style}
-        onClick={this.onLabelClick.bind(this) }>
+        onClick={this.onLabelClick }>
         {label}
       </span>
 
       <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={this.OnMarkImportant}>
         <i className="fa fa-exclamation" />
       </button>
 
       <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
+              className="btn btn-outline-danger btn-sm float-right"
+              onClick={onDeleted}>
+        <i className="fa fa-trash-o"/>
       </button>
     </span>
     );
